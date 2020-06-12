416ee3fc4c452b62ab1cb215b89f14342ffea87e
iqbal choironi <codenameich@gmail.com>
2020-06-12 23:27:10 +0700

    add register user

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./models/user');
+
 const mongosee = require('mongosee');
 mongosee.connect('',{useNewUrlParser: true})
     .then(() => {
@@ -8,8 +13,17 @@ mongosee.connect('',{useNewUrlParser: true})
     })
     .catch( err => console.log(err))
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userData) =>{
+        if(err) return res.json({ success: false, err})
+    })
+    return res.status(200)
 })
 
 app.listen(5000);
